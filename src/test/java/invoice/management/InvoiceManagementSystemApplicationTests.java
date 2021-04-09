80df4e178965188b7e2d0f0f0b67c0e7a5831977
jar-us <suraj.sharma3963@gmail.com>
2021-04-09 23:17:56 +0530

    1st commit

---
+++ src/test/java/invoice/management/InvoiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package invoice.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
